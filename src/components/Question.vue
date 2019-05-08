dfb346545c34d213dd019f9d78821827d558888c
Calua de Lacerda Pataca <calua.pataca@gmail.com>
2019-05-08 15:36:49 -0300

    content structure and basic styling for floating footer

--- src/components/Question.vue
+++ src/components/Question.vue
@@ -1,12 +1,33 @@
 <template>
   <div>
-    <p>{{this.$route.params.index}}</p>
-    <router-link :to="'/quest/' + (parseInt(this.$route.params.index)+1)">link</router-link>
+    <article id="quest">
+      <header>
+        <div class="text-headings">
+          <h1>{{json.form_heading}}</h1>
+          <h2>{{json.form_sub_heading}}</h2>
+        </div>
+        <div class="progress">
+          <p>40%</p>
+          <div class="progress-bar">
+            <div class="completed-rate"></div>
+          </div>
+        </div>
+      </header>
+    </article>
+
+    <!-- <p>{{this.$route.params.index}}</p>
+    <router-link :to="'/quest/' + (parseInt(this.$route.params.index)+1)">link</router-link>-->
   </div>
 </template>
 
 <script>
+import mock_json from "@/assets/mock_data.json";
 export default {
+  data() {
+    return {
+      json: mock_json
+    };
+  },
   name: "Question"
 };
 </script>
