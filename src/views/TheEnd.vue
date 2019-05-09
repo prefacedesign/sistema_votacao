4860757a81f0d0db8d3fa69e2bc7254f77d2e6cb
Calua de Lacerda Pataca <calua.pataca@gmail.com>
2019-05-09 15:20:47 -0300

    example of a vote code

--- src/views/TheEnd.vue
+++ src/views/TheEnd.vue
@@ -13,6 +13,14 @@
           </div>
         </div>
       </header>
+      <div class="vote-code">
+        <p>Seu voto foi registrado. Associada a ele, foi criada uma chave única.</p>
+        <p>Quando os resultados forem publicados você poderá usar essa mesma chave para confirmar se seu voto foi computado.</p>
+        <p class="hash">
+          Por favor, anote sua chave:
+          <span>{{secretKey}}</span>
+        </p>
+      </div>
       <p
         class="big-font"
         v-for="(paragraph, index) in json.ending.text"
@@ -28,7 +36,8 @@ import mock_json from "@/assets/mock_data.json";
 export default {
   data() {
     return {
-      json: mock_json
+      json: mock_json,
+      secretKey: "01d.ae3"
     };
   },
   name: "the-end"
