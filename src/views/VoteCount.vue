f3b44eb3269075a4c9edd1ef7429291162b50daa
Calua de Lacerda Pataca <calua.pataca@gmail.com>
2019-05-23 15:46:29 -0300

    table of votes with a little styling
    todo: order by hash.

--- src/views/VoteCount.vue
+++ src/views/VoteCount.vue
@@ -1,7 +1,23 @@
 <template>
   <div>
-    <p v-for="(vote, index) in votes" :key="'vote-' + index">{{vote}}</p>
-    <p v-for="(voter, index) in voters" :key="'voter-' + index">{{voter}}</p>
+    <header id="results">
+      <h1>{{json.form_heading}} / {{json.form_sub_heading}}</h1>
+      <h3>Compilação dos votos</h3>
+    </header>
+    <div class="results-compilation">
+      <table>
+        <tr>
+          <th>Hash do voto</th>
+          <th>Você concorda com a chapa apresentada?</th>
+        </tr>
+        <tr v-for="(vote, index) in prettyVotes" :key="'vote-' + index">
+          <td>{{vote.hash}}</td>
+          <td>{{vote.text}}</td>
+        </tr>
+      </table>
+    </div>
+    <!-- <p v-for="(vote, index) in votes" :key="'vote-' + index">{{vote}}</p> -->
+    <!-- <p v-for="(voter, index) in voters" :key="'voter-' + index">{{voter}}</p> -->
   </div>
 </template>
 
@@ -16,6 +32,20 @@ export default {
       voters: []
     };
   },
+  computed: {
+    prettyVotes: function() {
+      let auxVotes = [];
+      this.votes.forEach(vote => {
+        let text = this.findText(vote.attributes.answers.concorda_chapa);
+        auxVotes.push({
+          value: vote.attributes.answers.concorda_chapa,
+          text: text,
+          hash: this.prettyHash(vote.id)
+        });
+      });
+      return auxVotes;
+    }
+  },
   beforeRouteEnter: function(to, from, next) {
     const Votes = Parse.Object.extend("Votes");
     const Voters = Parse.Object.extend("Voters");
@@ -28,6 +58,18 @@ export default {
     });
   },
   methods: {
+    findText: function(value) {
+      let text = "Value not found";
+      this.json.content_flow[0].options.forEach(option => {
+        if (option.value == value) {
+          text = option.text;
+        }
+      });
+      return text;
+    },
+    prettyHash: function(hash) {
+      return hash.slice(0, 3) + "." + hash.slice(-3);
+    },
     setData(votes, voters) {
       this.votes = votes;
       this.voters = voters;
